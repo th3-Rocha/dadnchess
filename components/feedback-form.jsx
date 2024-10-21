52e71856d0a491c7c2d6e7bd313011cabaf2c87f
th3-rocha <murilorocha537@gmail.com>
2024-10-21 19:35:40 -0300

    ad

--- components/feedback-form.jsx
+++ components/feedback-form.jsx
@@ -34,23 +34,23 @@ export function FeedbackForm() {
     return (
         <div className=" container max-w-2xl  mx-auto">
             <h2 className="text-3xl font-bold mb-8 text-center">Entre em Contato</h2>
-            <form name="feedback" onSubmit={handleFormSubmit} className="text-black flex flex-col gap-3 align-center">
+            <form name="feedback" onSubmit={handleFormSubmit} className="text-black flex flex-col gap-3 align-center" netlify>
                 <input type="hidden" name="form-name" value="feedback" />
                 <input name="name" type="text" placeholder="Nome" required className="input input-bordered" />
-                <input name="email" type="text" placeholder="Email" required className="input input-bordered" />
+                <input name="email" type="text" placeholder="Email" className="input input-bordered" />
                 <input name="message" type="text" placeholder="Mensagem" required className="input input-bordered" />
                 <button className="w-full btn btn-primary mb-8" type="submit" disabled={status === 'pending'}>
                     Submit
                 </button>
 
                 {status === 'ok' && (
-                    <div className="alert alert-success">
+                    <div className="alert alert-success mb-8" >
                         <SuccessIcon />
                         Enviado!
                     </div>
                 )}
                 {status === 'error' && (
-                    <div className="alert alert-error">
+                    <div className="alert alert-error mb-8">
                         <ErrorIcon />
                         {error}
                     </div>
